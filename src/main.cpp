86705ba77e8aabe9632d87e909baa4fa3f47415e
Viltard Léo <leo.viltard@epitech.eu>
2023-11-27 17:36:17 +0100

    [+] opening a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,37 @@
-int main(int argc, char** argv) {
+/*
+ * Copyright (c) 2023 - Kleo
+ * Authors:
+ * - LEO VILTARD <leo.viltard@epitech.eu>
+ * NOTICE: All information contained herein is, and remains
+ * the property of Kleo © and its suppliers, if any.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Kleo ©.
+*/
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to init GLFW" << std::endl;
+        return 1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
