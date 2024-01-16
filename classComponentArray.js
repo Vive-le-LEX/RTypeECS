5cfd1d870822d102e24dd4d667f5eb312ec02b01
QuentinBriand <QuentinBriand@users.noreply.github.com>
2024-01-16 00:08:32 +0000

    deploy: 500dfc07cc58aa462f82fa495a3f58bdc3b68740

--- classComponentArray.js
+++ classComponentArray.js
@@ -3,6 +3,10 @@ var classComponentArray =
     [ "ComponentArray", "classComponentArray.html#aeeb456f2a6540fc08e88061ca27bbf21", null ],
     [ "copy", "classComponentArray.html#a3a2446f88bd9559d8b9cdfea7ea900d3", null ],
     [ "getComponent", "classComponentArray.html#a90071c521f4f704ea1eb476a458673f0", null ],
+    [ "getComponentArray", "classComponentArray.html#acd9f50a49b34c4883a4b59a87b4791bc", null ],
+    [ "getEntityToIndexMap", "classComponentArray.html#abaa85ada4751d244ca7cbefb16e218ab", null ],
+    [ "getIndexToEntityMap", "classComponentArray.html#acff4805e40bd76313cfb5021662ebc20", null ],
+    [ "getTotalSize", "classComponentArray.html#a9a97b6a6ed1f2cfb328ba917416ed96c", null ],
     [ "insert", "classComponentArray.html#a8c61f46e039461621cc975349badebcb", null ],
     [ "remove", "classComponentArray.html#a9764a18afcba741091e958fdca89e83e", null ],
     [ "safeRemove", "classComponentArray.html#a643fb6169f1ef5189554fa1df8896355", null ]
--- classComponentManager.js
+++ classComponentManager.js
@@ -4,7 +4,11 @@ var classComponentManager =
     [ "copyComponent", "classComponentManager.html#a9f61d0d5cc06ef82f14694c7a6a0352c", null ],
     [ "destroyEntity", "classComponentManager.html#a40940f494f117d758f4f4197def357bc", null ],
     [ "getComponent", "classComponentManager.html#a0e5b058c1df54690eb2af7167bda7fc8", null ],
+    [ "getComponentArrayPtr", "classComponentManager.html#a72f2659663bf766de078d678ebafc260", null ],
     [ "getComponentType", "classComponentManager.html#a6f5c040f73e5007026ae99bc5f0ac589", null ],
+    [ "getEntityToIndexMap", "classComponentManager.html#a05302a0dbc87b2a10013025b3b4bb47c", null ],
+    [ "getIndexToEntityMap", "classComponentManager.html#a1a2828eb104725aac1ed41647b9a3894", null ],
+    [ "getTotalSize", "classComponentManager.html#aede4784e4e6b87fe1539efab250be4bd", null ],
     [ "registerComponent", "classComponentManager.html#a3e5c5c9c56ca50e379fb48a3c0776500", null ],
     [ "removeComponent", "classComponentManager.html#a19d1702f671c37c565816f508080f343", null ]
 ];
