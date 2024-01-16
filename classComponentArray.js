3867cd2de760c2ab8d341ccf194bd19e968639fa
QuentinBriand <QuentinBriand@users.noreply.github.com>
2024-01-16 14:57:15 +0000

    deploy: 89048a086715c9e470ddf31fbe0d27b006d78557

--- classComponentArray.js
+++ classComponentArray.js
@@ -3,10 +3,11 @@ var classComponentArray =
     [ "ComponentArray", "classComponentArray.html#aeeb456f2a6540fc08e88061ca27bbf21", null ],
     [ "copy", "classComponentArray.html#a3a2446f88bd9559d8b9cdfea7ea900d3", null ],
     [ "getComponent", "classComponentArray.html#a90071c521f4f704ea1eb476a458673f0", null ],
-    [ "getComponentArray", "classComponentArray.html#acd9f50a49b34c4883a4b59a87b4791bc", null ],
+    [ "getComponentArray", "classComponentArray.html#a535f052b0cc016b8dc3ed3cec720ec06", null ],
     [ "getEntityToIndexMap", "classComponentArray.html#abaa85ada4751d244ca7cbefb16e218ab", null ],
     [ "getIndexToEntityMap", "classComponentArray.html#acff4805e40bd76313cfb5021662ebc20", null ],
     [ "getTotalSize", "classComponentArray.html#a9a97b6a6ed1f2cfb328ba917416ed96c", null ],
+    [ "hasComponent", "classComponentArray.html#a5428c651b5d7955cbdd367000dae6de3", null ],
     [ "insert", "classComponentArray.html#a8c61f46e039461621cc975349badebcb", null ],
     [ "remove", "classComponentArray.html#a9764a18afcba741091e958fdca89e83e", null ],
     [ "safeRemove", "classComponentArray.html#a643fb6169f1ef5189554fa1df8896355", null ]
--- classComponentManager.js
+++ classComponentManager.js
@@ -4,11 +4,12 @@ var classComponentManager =
     [ "copyComponent", "classComponentManager.html#a9f61d0d5cc06ef82f14694c7a6a0352c", null ],
     [ "destroyEntity", "classComponentManager.html#a40940f494f117d758f4f4197def357bc", null ],
     [ "getComponent", "classComponentManager.html#a0e5b058c1df54690eb2af7167bda7fc8", null ],
-    [ "getComponentArrayPtr", "classComponentManager.html#a72f2659663bf766de078d678ebafc260", null ],
+    [ "getComponentArrayPtr", "classComponentManager.html#a26851c6939bee8f98091109ee9e96bd1", null ],
     [ "getComponentType", "classComponentManager.html#a6f5c040f73e5007026ae99bc5f0ac589", null ],
-    [ "getEntityToIndexMap", "classComponentManager.html#a05302a0dbc87b2a10013025b3b4bb47c", null ],
-    [ "getIndexToEntityMap", "classComponentManager.html#a1a2828eb104725aac1ed41647b9a3894", null ],
+    [ "getEntityToIndexMap", "classComponentManager.html#a2b471773aca28728701dec0bb8c79e2c", null ],
+    [ "getIndexToEntityMap", "classComponentManager.html#a7b3658e2d065e4bf169541a225e401b7", null ],
     [ "getTotalSize", "classComponentManager.html#aede4784e4e6b87fe1539efab250be4bd", null ],
+    [ "hasComponent", "classComponentManager.html#a81f1478e824e9673bc2aab5cb82b3ebb", null ],
     [ "registerComponent", "classComponentManager.html#a3e5c5c9c56ca50e379fb48a3c0776500", null ],
     [ "removeComponent", "classComponentManager.html#a19d1702f671c37c565816f508080f343", null ]
 ];
